30fa1a3578d626577ffdc3d92eb58b90b18ee182
Gabriel Fernandes Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-09-16 11:04:11 -0300

    Update 09-fibonacci.cpp

--- PA (Programming and algorithms)/first-year/cpp/lis-1.cpp/09-fibonacci.cpp
+++ PA (Programming and algorithms)/first-year/cpp/lis-1.cpp/09-fibonacci.cpp
@@ -1,8 +1,8 @@
 /*
 * Date: 01/05/2026
 * Description:
-* Ler um número entre 50 e 100.
-* Exibir a sequência de Fibonacci até esse número.
+* Read a number between 50 and 100.
+* Display the Fibonacci sequence up to that number.
 */
 
 #include <iostream>
@@ -11,23 +11,23 @@ using namespace std;
 int main() {
     int limit;
 
-    cout << "Escreva um numero entre 50 e 100: ";
+    cout << "Write a number between 50 and 100: ";
     cin >> limit;
 
-    //verificar se o número está entre 50 e 100
+    //check if the number is between 50 and 100
     if (limit < 50 || limit > 100) {
-        cout << "Invalid number!" << endl;
+        cout << "Invalid number! " << endl;
         return 0;
     }
 
-    int ant = 0, atual = 1, prox;
+    int previous = 0, current = 1, next;
 
-    //calcular o fibonacci
-    while (ant <= limit) {
-        cout << ant << " ";
-        prox = ant + atual;
-        ant = atual;
-        atual = prox;
+    //calculate the Fibonacci sequence
+    while (previous <= limit) {
+        cout << previous << " ";
+        next = previous + current;
+        previous = current;
+        current = next;
     }
 
     cout << endl;
