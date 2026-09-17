7ab66a399bfe603a33f11302e149a7623898460f
Gabriel Fernandes Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-09-16 23:03:25 -0300

    Update main.cpp

--- PA (Programming and algorithms)/first-year/cpp/list-2.cpp/02-power.cpp/main.cpp
+++ PA (Programming and algorithms)/first-year/cpp/list-2.cpp/02-power.cpp/main.cpp
@@ -1,1 +1,30 @@
-a
+/*
+* Date: 30/05/2026
+* Description:
+* Read the base and the exponent. Calculate the power using repetition.
+* Create a function for the calculation.
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int pon() {
+    int base, power, mult = 1;
+  
+    cin >> base;
+    cin >> power;
+    for (int i=0; i<power; i++) {
+        mult*=base;
+    }
+    cout << multi;
+    return 0;
+}
+
+// main
+int main() {
+    pon();
+    return 0;
+}
